79f2a43a532751a3b52591cb33d2a0a454ba12f6
garrett <archangelgarrett@gmail.com>
2022-01-23 19:39:37 +0800

    gitignore

---
+++ RPS/client/js/client.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+const writeEvent = (text) => {
+    const parent = document.querySelector('#events');
+
+    const el = document.createElement('li');
+    el.innerHTML = text;
+
+    parent.appendChild(el);
+};
+
+const onFormSubmitted = (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector('#chat')
+    const text = input.value;
+    input.value = '';
+
+    sock.emit('message', text);
+};
+
+writeEvent('Welcome to my RPS');
+
+const sock = io();
+sock.on('message', writeEvent);
+
+document.querySelector('#chat-form').addEventListener('submit', onFormSubmitted);
